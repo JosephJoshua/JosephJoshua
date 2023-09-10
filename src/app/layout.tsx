7b3f15d1e005768219a1a749615b568375e66011
JosephJoshua <jj.anggita@gmail.com>
2023-09-10 13:02:58 +0700

    feat: add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,23 @@ const poppins = Poppins({
 export const metadata = {
   title: 'Joseph Joshua Anggita | Full-stack developer',
   description: 'Full-stack developer specializing in building exceptional digital experiences.',
+  keywords: [
+    'Joseph Joshua Anggita',
+    'software engineer',
+    'software engineer in Jakarta',
+    'software engineer in Indonesia',
+    'software developer in Jakarta',
+    'software developer in Indonesia',
+    'freelancer in Jakarta',
+    'freelancer in Indonesia',
+  ],
+  openGraph: {
+    title: 'Joseph Joshua Anggita | Full-stack developer',
+    url: process.env.VERCEL_URL,
+    type: 'website',
+    locale: 'en_US',
+    description: 'Full-stack developer specializing in building exceptional digital experiences.',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
